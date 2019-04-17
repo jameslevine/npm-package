be7436caad0e6b0e2d4270f56c8a54c44de77e6e
James Levine <jamesalexanderlevine@gmail.com>
2019-04-17 20:35:58 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# String Replacer NPM Package
+
+Put in a string and get back a string without the spaces!
+
+All you need to do is call the function stringReplacer with an argument including your string:
+
+`stringReplacer(yourString)`
+
+Enjoy!
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = function tiny(string) {
-  if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+module.exports = function removeSpaces(string) {
+  if (typeof string !== "string") throw new TypeError("This function needs a string!");
   return string.replace(/\s/g, "");
 };
